34c70c3cc8cc81f24bbf5f5dc859608a32a4b2dd
ariake <74305910+kyh31876@users.noreply.github.com>
2021-03-21 11:07:49 +0900

    Add files via upload

---
+++ 21-3.R
@@ -0,0 +1,66 @@
+english <- c(90, 80, 60, 70) # 영어 점수 변수 생성
+english
+
+math <- c(50, 60, 100, 20) # 수학 점수 변수 생성
+math
+
+
+# english, math로 데이터 프레임 생성해서 df_midterm에 할당
+df_midterm <- data.frame(english, math) 
+df_midterm
+
+class <- c(1, 1, 2, 2)
+class
+
+df_midterm <- data.frame(english, math, class)
+df_midterm
+
+mean(df_midterm$english) # df_midterm의 english로 평균 산출
+
+mean(df_midterm$math) # df_midterm의 math로 평균 산술
+
+df_midterm <- data.frame(english = c(90, 80, 60, 70),
+                         math = c(50, 60, 100, 20),
+                         class = c(1, 1, 2, 2)) #데이터 프레임 한꺼번에 만들기 
+df_midterm
+
+dim(df_midterm) #차원 확인 
+df_midterm[ , ] #원하는 행렬 선택 
+rm(df_midterm) #rm 함수는 원하는 object 지울떄 사용 
+
+
+#데이터 불러오기
+install.packages("readxl");
+getwd(); #현재 폴더 확인 
+
+library(readxl); # readxl 이란 library 생성 
+# 엑셀 파일을 불러와서 df_exam에 할당
+df_exam <- read_excel("/Users/yuhyun/Downloads/excel_exam.xlsx"); 
+
+df_exam <- read_excel(file.choose())
+df_exam
+
+head(df_exam) #앞부분만 확인 
+
+#엑셀 파일 첫 번째 행이 변수명이 아닐때
+df_exam_novar <- read_excel("/Users/yuhyun/Downloads/excel_exam_novar.xlsx", col_names = F)
+#기존에 있는 변수의 이름을 바꿀때
+names(df_exam) <- c("id","class","math","english","science")
+
+#엑셀 파일에 시트가 여러 개 있을때
+df_exam_sheet <- read_excel("/Users/yuhyun/Downloads/excel_exam_sheet.xlsx", sheet = 3)
+
+df_csv_exam <- read.csv("/Users/yuhyun/Downloads/csv_exam.csv" )
+
+#4번째 행부터 불러오기 
+df_csv_exam <- read.csv("/Users/yuhyun/Downloads/csv_exam.csv", skip=4 )
+
+#문자가 들어 있는 파일을 불러올 때
+
+df_csv_exam <- read.csv("/Users/yuhyun/Downloads/csv_exam.csv", stringsAsFactors = F)
+
+#data 를 csv형태로 저장 
+write.csv(rivers, file="/Users/yuhyun/Downloads/rivers.csv")
+
+data$X #번호
+data$x #rivers 
